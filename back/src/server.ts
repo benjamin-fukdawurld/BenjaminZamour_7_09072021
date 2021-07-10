729dba00b3b0a98b7720a9e1988efd3ab3e04370
benjamin <benjamin.zamour@gmail.com>
2021-07-10 14:23:11 +0200

    back/src/server.ts: Add node server

---
+++ back/src/server.ts
@@ -0,0 +1,60 @@
+import dotenv from 'dotenv';
+import https from 'https';
+import fs from 'fs';
+
+import { createApp } from './app';
+
+dotenv.config();
+
+function normalizePort(port: string | number): number {
+  let value = 0;
+  if (typeof port === 'string') {
+    value = parseInt(port, 10);
+  } else {
+    value = port;
+  }
+
+  if (Number.isNaN(value) || value <= 0) {
+    throw new Error(`Port value must be a positive integer, received '${value}'`);
+  }
+
+  return value;
+}
+
+const port = normalizePort(process.env.SERVER_PORT ?? 5000);
+
+const options = {
+  key: fs.readFileSync(process.env.HTTPS_KEY ?? 'key.pem'),
+  cert: fs.readFileSync(process.env.HTTPS_CERTIFICATE ?? 'cert.pem'),
+};
+
+const server = https.createServer(options, createApp({}));
+
+server.on('error', (error: Error | any) => {
+  if (error.syscall && error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const address = server.address();
+  const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges.`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use.`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+});
+
+server.on('listening', async () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`;
+  console.log(`Listening on ${bind}`);
+});
+
+server.listen(port);
