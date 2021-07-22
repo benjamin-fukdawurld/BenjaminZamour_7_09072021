b6a48f0536b76f26d9bc134ca04dc154796533ec
benjamin <benjamin.zamour@gmail.com>
2021-07-23 00:27:06 +0200

    back/src/controllers/user.ts: Add getUserProfile + Fix update user

--- back/src/controllers/user.ts
+++ back/src/controllers/user.ts
@@ -5,12 +5,15 @@ import assert from 'assert';
 
 import pool from '../database';
 import { User } from '../models/User';
+import { schema as passwordSchema } from '../middlewares/password-check';
+
+import { DepartmentController } from '../controllers/department';
 
 assert(process.env.TOKEN_KEY);
 
 class UserController {
-  private static async queryUser(id: number): Promise<User | null> {
-    const user = await pool.query('SELECT id, login FROM employee WHERE id = $1;', [id]);
+  private static async queryUser(id: number, fields = ['id', 'login']): Promise<User | null> {
+    const user = await pool.query(`SELECT ${fields.join(', ')} FROM employee WHERE id = $1;`, [id]);
 
     if (!user || user?.rows.length === 0) {
       return null;
@@ -46,11 +49,36 @@ class UserController {
     }
   }
 
+  public async getUserProfile(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const user = await pool.query(
+        `
+        SELECT email, login, password, privilege, firtName, lastName, jobTitle, birthDate, biography,
+          (SELECT name as department FROM department WHERE id = departmentId)
+        FROM employee
+        WHERE id = $1;
+        `,
+        [id],
+      );
+
+      if (!user || user?.rows.length === 0) {
+        res.status(404).send(`User '${id}' not found`);
+        return;
+      }
+
+      res.status(200).send(user.rows[0]);
+    } catch (err) {
+      res.status(500).send({ message: 'Unable to get this department' });
+      console.error(JSON.stringify(err));
+    }
+  }
+
   public async updateUser(req: Request, res: Response) {
     const { id } = req.params;
 
     try {
-      const user = await UserController.queryUser(parseFloat(id));
+      const user = await UserController.queryUser(parseFloat(id), ['id', 'login', 'password']);
 
       if (!user) {
         res.status(404).send({ message: `user '${id}' not found` });
@@ -58,11 +86,41 @@ class UserController {
       }
 
       const columns = Object.keys(req.body).filter(
-        (column: string) => ['login', 'email', 'password'].includes(column) && req.body[column],
+        (column: string) =>
+          [
+            'login',
+            'email',
+            'password',
+            'firstName',
+            'lastName',
+            'jobTitle',
+            'birthDate',
+            'biography',
+          ].includes(column) && req.body[column],
       );
 
       if (columns.includes('password')) {
         await argon2.verify(user.password, req.body.oldPassword);
+        const failedRules = passwordSchema.validate(req.body.password, { list: true });
+        if (failedRules.length !== 0) {
+          res.status(400).send({ message: 'Password is not strong enough', failedRules });
+          return;
+        }
+
+        req.body.password = await argon2.hash(req.body.password, {
+          type: argon2.argon2id,
+        });
+      }
+
+      if (req.body.department) {
+        const department = await DepartmentController.queryDepartmentByName(req.body.department);
+        if (!department) {
+          res.status(404).send({ message: `Department '${req.body.department}' not found` });
+          return;
+        }
+
+        columns.push('departmentId');
+        req.body.departmentId = department.id;
       }
 
       await pool.query(
@@ -71,7 +129,7 @@ class UserController {
           .join(', ')} WHERE id = $1;`,
         [id, ...columns.map((column: string) => req.body[column])],
       );
-      res.status(200).send({ message: `Department '${req.body.login}' updated` });
+      res.status(200).send({ message: `User '${id}' updated` });
     } catch (err) {
       res.status(500).send({ message: 'Unable to update this user' });
       console.error(JSON.stringify(err));
