23600b2f0b503be8038538f36a5be0660df4850e
benjamin <benjamin.zamour@gmail.com>
2021-07-23 16:33:10 +0200

    controllers/user.ts: Add authentification middleware to user routes

--- back/src/routes/user.ts
+++ back/src/routes/user.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
 
 import controller from '../controllers/user';
+
 import passwordChecker from '../middlewares/password-check';
+import auth from '../middlewares/auth';
 
 const router = Router();
 
-router.get('/', controller.getUsers.bind(controller));
-router.get('/:id', controller.getUser.bind(controller));
+router.get('/', auth(1), controller.getUsers.bind(controller));
+router.get('/:id', auth(), controller.getUser.bind(controller));
 router.post('/signup', passwordChecker, controller.signup.bind(controller));
 router.post('/login', controller.login.bind(controller));
-router.patch('/', controller.updateUser.bind(controller));
-router.delete('/:id', controller.deleteUser.bind(controller));
-router.get('/:id/profile', controller.getUserProfile.bind(controller));
+router.patch('/:id', auth(), controller.updateUser.bind(controller));
+router.delete('/:id', auth(), controller.deleteUser.bind(controller));
+router.get('/:id/profile', auth(), controller.getUserProfile.bind(controller));
 
 export default router;
