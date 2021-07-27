03c353e5a438fc916ea2dc387a6dcbc4e86d88be
Benjamin ZAMOUR <bzamour@mediarithmics.com>
2021-07-27 19:58:46 +0200

    back/src/database.ts: Update database connexion to match new env variables

--- back/src/database.ts
+++ back/src/database.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
+  user: process.env.POSTGRES_CLIENTUSER,
+  password: process.env.POSTGRES_CLIENTPASSWORD,
   host: process.env.POSTGRES_HOST,
   port: parseFloat(process.env.POSTGRES_PORT ?? '5432'),
-  database: process.env.POSTGRES_DATABASE,
+  database: process.env.POSTGRES_DB,
 });
 
 export default pool;
