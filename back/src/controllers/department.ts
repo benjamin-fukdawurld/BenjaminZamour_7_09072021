c15f625e36ce7b31fda7e0034617ff8010740081
benjamin <benjamin.zamour@gmail.com>
2021-07-28 23:06:31 +0200

    back/controller/department.ts: Fix missing await in getDepartment

--- back/src/controllers/department.ts
+++ back/src/controllers/department.ts
@@ -1,116 +1,70 @@
 import { Request, Response } from 'express';
-import pool from '../database';
-import { Department } from '../models/Department';
+import service from '../services/DepartmentService';
+import logger from '../common/logger';
 
 export class DepartmentController {
-  private static formatName(name: string): string {
-    return name
-      .trim()
-      .replace(/ +/g, ' ')
-      .split(' ')
-      .map((s: string): string => s.charAt(0).toUpperCase() + s.substring(1))
-      .join(' ');
-  }
-
-  private static async queryDepartment(id: number): Promise<Department | null> {
-    const department = await pool.query('SELECT * FROM department WHERE id = $1;', [id]);
-
-    if (!department || department?.rows.length === 0) {
-      return null;
-    }
-
-    return department.rows[0];
-  }
-
-  public static async queryDepartmentByName(name: string): Promise<Department | null> {
-    const department = await pool.query('SELECT * FROM department WHERE name = $1;', [
-      DepartmentController.formatName(name),
-    ]);
-
-    if (!department || department?.rows.length === 0) {
-      return null;
-    }
-
-    return department.rows[0];
-  }
-
   public async getDepartments(req: Request, res: Response) {
     try {
-      const departments = await pool.query('SELECT * FROM department;');
-      res.status(200).send(departments?.rows);
+      const serviceResponse = await service.getDepartments();
+      res.status(serviceResponse.status).send(serviceResponse.result);
     } catch (err) {
-      res.status(500).send({ message: 'Unable to get department list' });
-      console.error(JSON.stringify(err));
+      res.status(500).send(err);
+      logger.error(JSON.stringify(err));
     }
   }
 
   public async getDepartment(req: Request, res: Response) {
     const { id } = req.params;
     try {
-      const department = DepartmentController.queryDepartment(parseFloat(id));
-
-      if (!department) {
-        res.status(404).send(`Department '${id}' not found`);
-        return;
-      }
-
-      res.status(200).send(department);
+      const serviceResponse = await service.getDepartment(parseFloat(id));
+      res.status(serviceResponse.status).send(serviceResponse.result);
     } catch (err) {
-      res.status(500).send({ message: 'Unable to get this department' });
-      console.error(JSON.stringify(err));
+      res.status(500).send(err);
+      logger.error(JSON.stringify(err));
     }
   }
 
   public async addDepartment(req: Request, res: Response) {
-    let { name } = req.body;
+    const { name } = req.body;
     if (!name) {
       res.send(400).send({ message: 'Missing department name' });
       return;
     }
 
-    name = DepartmentController.formatName(name);
-
     try {
-      await pool.query('INSERT INTO department(name) VALUES ($1)', [name]);
-      res.status(201).send({ message: `Department '${name}' added` });
+      const serviceResponse = await service.createDepartment({ name });
+      res.status(serviceResponse.status).send(serviceResponse.result);
     } catch (err) {
-      res.status(500).send({ message: `Department '${name}' insertion failed` });
-      console.error(JSON.stringify(err));
+      res.status(500).send(err);
+      logger.error(JSON.stringify(err));
     }
   }
 
   public async updateDepartment(req: Request, res: Response) {
-    const { id, name } = req.body as Department;
-    try {
-      const formattedName = DepartmentController.formatName(name);
-      const department = await DepartmentController.queryDepartment(id);
-      if (!department) {
-        res.status(404).send({ message: `department '${id}' not found` });
-        return;
-      }
+    const { id } = req.params;
+    const { name } = req.body;
+    if (!id || !name) {
+      res.status(400).send({ message: 'Missing information' });
+      return;
+    }
 
-      await pool.query('UPDATE department SET name = $2 WHERE id = $1;', [id, formattedName]);
-      res.status(200).send({ message: `Department '${formattedName}' updated` });
+    try {
+      const serviceResponse = await service.updateDepartment(parseFloat(id), { name });
+      res.status(serviceResponse.status).send(serviceResponse.result);
     } catch (err) {
-      res.status(500).send({ message: 'Unable to update this department' });
-      console.error(JSON.stringify(err));
+      res.status(500).send(err);
+      logger.error(JSON.stringify(err));
     }
   }
 
   public async deleteDepartment(req: Request, res: Response) {
     const { id } = req.params;
     try {
-      const department = await DepartmentController.queryDepartment(parseFloat(id));
-      if (!department) {
-        res.status(404).send({ message: `department '${id}' not found` });
-        return;
-      }
-
-      await pool.query('DELETE FROM department WHERE id = $1;', [id]);
-      res.status(200).send({ message: `Department '${id}' deleted` });
+      const serviceResponse = await service.getDepartment(parseFloat(id));
+      res.status(serviceResponse.status).send(serviceResponse.result);
     } catch (err) {
-      res.status(500).send({ message: 'Unable to delete this department' });
-      console.error(JSON.stringify(err));
+      res.status(500).send(err);
+      logger.error(JSON.stringify(err));
     }
   }
 }
